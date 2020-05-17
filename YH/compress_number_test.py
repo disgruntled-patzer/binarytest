fb2fb3a573dc58c870085530c2506e739fed9879
Lau Yan Han <sps08.lauyanhan@gmail.com>
2020-05-17 14:38:36 +0800

    Add GNU-GPL-v3 license

--- YH/compress_number_test.py
+++ YH/compress_number_test.py
@@ -2,6 +2,21 @@
 
 # Attempts to binary compress a regular payload with all its accompanying prefixes to be sent to a Rockblock 9603
 
+# Copyright (C) 2020, Lau Yan Han (sps08.lauyanhan@gmail.coms)
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>
+
 import binascii
 import struct
 import time
