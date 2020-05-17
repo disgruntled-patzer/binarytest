a14dc3fefaf05cd4051dd3fb4b7ee2b259612a2a
Lau Yan Han <sps08.lauyanhan@gmail.com>
2020-05-17 14:36:33 +0800

    Add code for unpacking

--- YH/compress_number_test.py
+++ YH/compress_number_test.py
@@ -6,55 +6,42 @@ import binascii
 import struct
 import time
 
+# Pack Prefix
+pre_1 = (b'RB',)
+s1 = struct.Struct('2s')
+packed_1 = s1.pack(*pre_1)
+pre_2 = (19469,)
+s2 = struct.Struct('> I')
+packed_2 = s2.pack(*pre_2)
+packed_2 = packed_2[1:]
+
 # Original msg
 msg = "19 259 1 21 2 2304 102 6798 4 0 10"
-serial = 19466
 
-# Add prefixes
-msg = "RB " + str(serial) + " R " + str(int(time.time())) + " " + msg
-print ("Original msg (with prefixes): " + msg)
-print ("Original msg length: " + str(len(msg)))
-
-# Spilt the msg into a list (can we tell the despatcher node to send us a list instead of a string...?)
+# Spilt the msg into a list
 li = msg.split(" ")
+li = list(map(int, li))
 li_size = len(li)
 
-# Prepare the prefixes.
-# 1st entry: RB (2 characters)
-# 2nd entry: Rockblock serial number (unsigned short int, assuming serial doesn't exceed 65535)
-# 3rd entry: R (to identify payload as regular payload. 1 character)
-# 4th entry: 32 bit (4 byte) UNIX timestamp
-# Preparing the 1st entry (RB, which are 2 characters)
-li[0] = li[0].encode()
-li[1] = int(li[1])
-li[2] = li[2].encode()
-li[3] = int(li[3])
-struct_cmd = '2s H s I'
-
-# Preparing the rest of the payload (which are all short ints) 
-i = 4
+# Preparing the regular payload (which are all short ints) 
+i = 0
+struct_cmd = '>'
 while i < li_size:
 	struct_cmd = struct_cmd + ' H'
-	li[i] = int(li[i])
 	i = i + 1
 
-# Pack the data
+# Pack the regular payload
 s = struct.Struct(struct_cmd)
-packed_data = s.pack(*li)
+packed_3 = s.pack(*li)
+packed_data = packed_1 + packed_2 + packed_3
 print(packed_data)
-print("Packed msg length: " + str(s.size))
-
-# 56 byte regular payload in text format becomes 34 bytes in compressed format. Yay!
-# Note that total length of the struct is not equal to sum of sizeof each individual entry
-# See https://www.geeksforgeeks.org/is-sizeof-for-a-struct-equal-to-the-sum-of-sizeof-of-each-member/
-
-# Calculate Checksum. Looks deceptively easy; I'll test this on a Rockblock tomorrow...
-checksum = sum(packed_data)
-print("Checksum: " + str(checksum))
-higher_byte = checksum >> 8
-lower_byte = checksum & 0xFF
-print(higher_byte)
-print(lower_byte)
+print(binascii.hexlify(packed_data))
+print("Packed msg length: " + str(s1.size + s2.size-1 + s.size))
 
 # Now unpack it
-print(s.unpack(packed_data))
+
+# Unpack if it is binary
+if packed_data[0] == ord('R') and packed_data[1] == ord('B'):
+	packed_data = packed_data[5:]
+	unpacked_data = struct.unpack(struct_cmd, packed_data)
+	print(unpacked_data)
